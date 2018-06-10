e555b3edfab23284ed6cc0e03cdc0050f518813e
nagasuresh <nagasuresh92@hotmail.com>
2018-06-10 19:03:51 +0700

    learnt how to reuse methods in python using inheritence and method overridind
    learnt how to reuse methods in python using inheritence and method overriding

--- inheritence.py
+++ inheritence.py
@@ -11,13 +11,23 @@ class Parent():
         self.last_name=last_name
         self.eye_color=eye_color
         
+    def show_info(self):
+        print("last name-"+self.last_name)
+        print("eye color-"+self.eye_color)
+        
+        
         
 class Child(Parent):
     def __init__(self,last_name,eye_color,number_of_toys):
         print("Calling Child class Constructor")
         Parent.__init__(self,last_name,eye_color)
         self.number_of_toys=number_of_toys
+    
+    def show_info(self):
+        print("last name-"+self.last_name)
+        print("eye color-"+self.eye_color)
+        print("no of toys"+str(self.number_of_toys))
            
         
-suresh_jr=Child("donda","blue",600)
-print(suresh_jr.number_of_toys)       
+suresh_jr=Child("donda","blue",12)
+print(suresh_jr.show_info())       
